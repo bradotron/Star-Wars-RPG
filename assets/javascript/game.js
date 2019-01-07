66b7da48dd05f4315cc9ca4b464ff139a011849b
Bradley Jensen <bradley13@gmail.com>
2019-01-07 18:16:38 -0500

    added instructions to folder

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,37 @@
 // simple star wars game written with jQuery
 // Bradley Jensen
-//console.log("Hello World");
+// console.log("Brad was here...");
 
-$(document).ready(function() {
+// this line is a shortcut way to get the same effect as a document.ready
+$(function() {
   //console.log("document ready");
-  //$(document).keyup(userKeyUp(event));
 
-  // $(document).on("keyup", function(event) {
-  //   console.log(event);
-  // } );
+  var i = 0;
+  // the setInterval(function, delay) runs the function every delay milliseconds
+  setInterval(function(){
+    console.log(i);
+    i = (i+1) % 10; // this line creates a looping i up to the value behind the modulus operator
+  }, 1000);
 
-  $(document).keyup(function(event) {
-    userKeyUp(event);
+  // create document listeners and link to the functions
+  $(document).keydown(function(event) {
+    userKeyDown(event);
   }); 
 
   $(document).click(function(event) {
     userClick(event);
-  })
+  });
+
+
+  // my functions go down here
+  function userKeyDown(e) {
+    // user released a key
+    console.log("Key Down");
+  }
+  
+  function userClick(e) {
+    // user clicked the mouse
+    console.log("Mouse Click");
+  }
 });
 
-function userKeyUp(e) {
-  // user released a key
-  console.log(e);
-}
-
-function userClick(e) {
-  console.log(e);
-  console.log(e.clientX + " " + e.clientY);
-}
